629a7c9b4480550cdc319bd4a2b7a2f13d31276e
DmitriyPrylutskyi <dprily@gmail.com>
2017-03-29 15:33:11 +0300

    Add searchform

---
+++ searchform.php
@@ -0,0 +1,10 @@
+<div class="widget">
+	<form action="">
+	  <div class="input-group">
+	    <input type="text" class="form-control" placeholder="Search for..." name="s">
+	    <span class="input-group-btn">
+	      <button class="btn btn-default" type="submit">Go!</button>
+	    </span>
+	  </div>
+	</form>  
+</div>
